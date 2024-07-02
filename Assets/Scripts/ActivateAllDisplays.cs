49263e7c9789ef65c81109b0bc189d49eebbc0eb
Deniz Yunus Göğüş <58386750+DenizYunus@users.noreply.github.com>
2024-07-02 18:59:51 +0300

    Latest

--- Assets/Scripts/ActivateAllDisplays.cs
+++ Assets/Scripts/ActivateAllDisplays.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.IO;
 
 public class ActivateAllDisplays : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class ActivateAllDisplays : MonoBehaviour
     public GameObject remoteImage1;
     public GameObject remoteImage2;
     public GameObject localImage;
+
+    public List<RawImage> imagesToBeTransformed;
 
     void Start()
     {
@@ -45,10 +49,94 @@ public class ActivateAllDisplays : MonoBehaviour
             rt.offsetMin = new Vector2(-350, -200);
             rt.offsetMax = new Vector2(0, 0);
         }
+
+        if (Display.displays.Length == 2)
+        {
+            LoadTransforms();
+        }
     }
 
-    void Update()
+    private void LoadTransforms()
     {
+        string path;
+#if UNITY_EDITOR
+        path = Path.Combine(Application.dataPath, "transforms.json");
+#else
+        path = Path.Combine(Application.dataPath, @"..\transforms.json");
+#endif
 
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            TransformCollection transformCollection = JsonUtility.FromJson<TransformCollection>(json);
+            ApplyTransforms(transformCollection);
+        }
+        else
+        {
+            Debug.LogError("Transform file not found at: " + path);
+        }
+    }
+
+    private void ApplyTransforms(TransformCollection transformCollection)
+    {
+        foreach (var transformData in transformCollection.images)
+        {
+            RawImage img = imagesToBeTransformed.Find(i => i.name == transformData.name);
+            if (img != null)
+            {
+                img.rectTransform.localPosition = transformData.position.ToVector3();
+                img.rectTransform.localRotation = transformData.rotation.ToQuaternion();
+                img.rectTransform.localScale = transformData.scale.ToVector3();
+                img.rectTransform.sizeDelta = new Vector2(transformData.width, transformData.height);
+            }
+            else
+            {
+                Debug.LogError("Image with name " + transformData.name + " not found in the canvas.");
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class TransformData
+    {
+        public string name;
+        public Vector3Data position;
+        public QuaternionData rotation;
+        public Vector3Data scale;
+        public float width;
+        public float height;
+    }
+
+    [System.Serializable]
+    public class Vector3Data
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3 ToVector3()
+        {
+            return new Vector3(x, y, z);
+        }
+    }
+
+    [System.Serializable]
+    public class QuaternionData
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public Quaternion ToQuaternion()
+        {
+            return new Quaternion(x, y, z, w);
+        }
+    }
+
+    [System.Serializable]
+    public class TransformCollection
+    {
+        public List<TransformData> images;
     }
 }
